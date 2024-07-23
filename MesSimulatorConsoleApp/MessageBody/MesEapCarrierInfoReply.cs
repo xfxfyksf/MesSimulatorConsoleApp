674f8f3354a5efae689874ec85941ac9b36ee456
Jianfeng Wu <695804696@qq.com>
2024-07-23 18:18:41 +0800

    添加项目文件。

---
+++ MesSimulatorConsoleApp/MessageBody/MesEapCarrierInfoReply.cs
@@ -0,0 +1,16 @@
+﻿using MessageSimulator.Core.Models.Message;
+
+namespace MesSimulatorConsoleApp.MessageBody
+{
+    [Serializable]
+    public class MesEapCarrierInfoReply : Body
+    {
+        public new const string MESSAGENAME = "MESEAP_CARRIER_INFO_REPLY";
+        public string? FACTORYNAME { get; set; }
+        public string? CARRIERNAME { get; set; }
+        public string? PORTNAME { get; set; }
+        public string? JOBUSETYPE { get; set; }
+        public string? PURGERECIPE { get; set; }
+        public string? SLOTMAP { get; set; }
+    }
+}
